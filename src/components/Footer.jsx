8c4e4cb99aa396e9d8c8a9272ce1f5c803c6245b
yujiyoung5457 <yujiyoung5457@gmail.com>
2026-07-31 13:07:26 +0900

    footer1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Footer.module.scss'
 
 const Footer = () => {
@@ -36,6 +37,5 @@ const Footer = () => {
 }
 
 export default Footer
-
 
 
